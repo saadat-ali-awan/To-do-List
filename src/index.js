b8159b74e8434e4aef1b3d7a415326cc999077e7
Saadat123456 <saadatlucky313@gmail.com>
2022-02-18 03:18:47 +0500

    Update index.js according to modules.

--- src/index.js
+++ src/index.js
@@ -1,41 +1,9 @@
 import './stylesheet/main.css';
-import threeDots from './images/three-dots.png';
-
-const todoList = [
-  {
-    index: 2,
-    completed: false,
-    description: 'Write Code',
-  },
-  {
-    index: 0,
-    completed: true,
-    description: 'Drink coffee and hear lecture',
-  },
-  {
-    index: 1,
-    completed: false,
-    description: 'Play Games',
-  },
-];
+import events from './js/events.js';
+import todolist from './js/todolist.js';
+import domElements from './js/dom_elements.js';
 
 window.addEventListener('load', () => {
-  const listElem = document.querySelector('#list');
-  todoList.sort((a, b) => {
-    if (a.index > b.index) {
-      return 1;
-    }
-    if (a.index < b.index) {
-      return -1;
-    }
-    return 0;
-  });
-
-  todoList.forEach((task) => {
-    listElem.innerHTML += `<li>
-    <input type="checkbox" name='completed' value='${task.completed ? 'completed' : 'not-completed'}' ${task.completed ? 'checked' : ''}>
-    <span>${task.description}</span>
-    <img src=${threeDots} alt="Three Dots Button">
-    </li>`;
-  });
+  todolist.init();
+  events.init(domElements);
 });
