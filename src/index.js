52483872fd251f8ca6f7c53471c2577ff050776e
Saadat123456 <saadatlucky313@gmail.com>
2022-02-17 06:09:55 +0500

    Dynamically load to-do list items

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
-import _ from 'lodash';
+import './stylesheet/main.css';
+import threeDots from './images/three-dots.png';
 
-function component() {
-  const element = document.createElement('div');
+const todoList = [
+  {
+    index: 0,
+    completed: false,
+    description: 'Write Code',
+  },
+  {
+    index: 1,
+    completed: true,
+    description: 'Drink coffee and hear lecture',
+  },
+  {
+    index: 2,
+    completed: false,
+    description: 'Play Games',
+  },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  return element;
-}
-
-document.body.appendChild(component());
+window.addEventListener('load', () => {
+  const listElem = document.querySelector('#list');
+  todoList.forEach((task) => {
+    listElem.innerHTML += `<li>
+    <input type="checkbox" name='completed' value='${task.completed ? 'completed' : 'not-completed'}' ${task.completed ? 'checked' : ''}>
+    <span>${task.description}</span>
+    <img src=${threeDots} alt="Three Dots Button">
+    </li>`;
+  });
+});
