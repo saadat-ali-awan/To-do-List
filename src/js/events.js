1dfb4ca424f483913d56a3b924a5a350c979f732
Saadat123456 <saadatlucky313@gmail.com>
2022-02-18 03:17:16 +0500

    Create Event class that would add event listeners

---
+++ src/js/events.js
@@ -0,0 +1,15 @@
+import todolist from './todolist.js';
+
+class Events {
+  init(domElements) {
+    this.domElements = domElements;
+    this.domElements.getInputElement().addEventListener('keyup', (event) => {
+      if (event.keyCode === 13) {
+        if (this.domElements.getInputElement().value !== '') {
+          todolist.addTask(false, this.domElements.getInputElement().value);
+        }
+      }
+    });
+  }
+}
+export default new Events();
