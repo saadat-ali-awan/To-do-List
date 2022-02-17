7a3b76ca2ecd7da24b27f33ed57cc80be5c6428d
Saadat123456 <saadatlucky313@gmail.com>
2022-02-17 08:42:05 +0500

    Add Delete Task Function

--- src/js/todolist.js
+++ src/js/todolist.js
@@ -8,4 +8,18 @@ export default class TodoList {
   addTask(completed, description) {
     this.list.add(Task(this.list.length, completed, description));
   }
+
+  deleteTask(index) {
+    this.sortTasks();
+    let newIndex = 0;
+    const newList = [];
+    this.list.forEach((task) => {
+      if (index !== task.index) {
+        newList.add(Task(newIndex, task.completed, task.description));
+        newIndex += 1;
+      }
+    });
+    this.list = newList;
+    this.saveToLocalStorage();
+  }
 }
