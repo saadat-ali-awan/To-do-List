aa00c3197c35a8d4069eafb9fdef2ed6ae3e0e17
Saadat123456 <saadatlucky313@gmail.com>
2022-02-17 08:41:12 +0500

    Add addTask function

--- src/js/todolist.js
+++ src/js/todolist.js
@@ -1,5 +1,11 @@
+import Task from './task.js';
+
 export default class TodoList {
   constructor() {
     this.list = [];
+  }
+
+  addTask(completed, description) {
+    this.list.add(Task(this.list.length, completed, description));
   }
 }
